8a5b0244c4ef00a39f0c44e3a5f7023adca46e2d
Farouk <farouk@orthlane.com>
2021-12-04 22:39:35 +0100

    redux

--- appComponents/redux/notification/notificationSlice.js
+++ appComponents/redux/notification/notificationSlice.js
@@ -16,13 +16,31 @@ const notificationSlice = createSlice({
       state.notification.notificationState = !state.notification.notificationState;
     },
     addToNotificationList(state,action) {
+
+
       state.notification.notificationList.push(action.payload);
+      state.notification.notificationList = eliminateRedundancy(state.notification.notificationList);
+
     },
     
 
 
   },
 });
+
+const eliminateRedundancy = (list) => {
+  if (list === undefined) {
+    return [];
+  }
+  let result = [];
+  for (let i = 0; i < list.length; i++) {
+    if (!result.some((item) => item.data.id === list[i].data.id)) {
+      result.push(list[i]);
+    }
+  }
+  return result;
+};
+
 
 export const { addToNotificationList,setNotificationState} = notificationSlice.actions;
 export default notificationSlice.reducer;
--- appComponents/screens/details/notificationList.js
+++ appComponents/screens/details/notificationList.js
@@ -2,50 +2,56 @@ const notificationList = [
     {
         title: "'December 03, 2021 18:29:00' ",
         body: 'notification body',
-        trigger: new Date('December 03, 2021 23:10:30')
+        trigger: new Date('December 03, 2021 23:10:30'),
+        data:{id:1},
     },
     {
         title: "'December 03, 2021 18:37:00' ",
         body: 'notification body',
-        trigger: new Date('December 03, 2021 23:10:40')
+        trigger: new Date('December 03, 2021 23:10:40'),
+        data:{id:2},
     },
     {
         title: "'December 03, 2021 18:37:00' ",
         body: 'notification body',
-        trigger: new Date('December 03, 2021 19:01:40')
+        trigger: new Date('December 03, 2021 19:01:40'),
+        data:{id:3},
     },
     {
         title: "taw' ",
         body: 'notification body',
         trigger: new Date('December 04, 2021 15:18:00'),
-        data:{screen:"Feedback"},
-        image: require('../../../assets/images/coronaReminder.png'),
+        data:{screen:"Feedback",id:4,
+        image: `../../../assets/images/coronaReminder.png`,},
+       
 
     },
     {
         title: "taw' ",
         body: 'notification body',
         trigger: new Date('December 04, 2021 15:23:00'),
-        data:{screen:"Feedback",image: require('../../../assets/images/coronaReminder.png')},
+        data:{screen:"Feedback",image: require('../../../assets/images/coronaReminder.png'),id:5},
 
 
     },   {
         title: "taw' ",
         body: 'notification body',
         trigger: new Date('December 04, 2021 15:16:00'),
-        data:{screen:"Feedback"},
+        data:{screen:"Feedback",image: require('../../../assets/images/coronaReminder.png'),id:6},
         image: require('../../../assets/images/coronaReminder.png'),
 
     },
     {
         title: "'November 19, 2021 13:19:00' ",
         body: 'notification body',
-        trigger: new Date('December 04, 2021 14:29:00')
+        trigger: new Date('December 04, 2021 14:29:00'),
+        data:{id:7},
     },
     {
         title: "'November 16, 2021 17:20:00'",
         body: 'notification body',
-        trigger: new Date('December 03, 2021 23:16:40')
+        trigger: new Date('December 03, 2021 23:16:40'),
+        data:{id:8},
     }
 ];
 
