6a7cbe9672d1125a1cf9212757dba065b9be654a
Farouk <farouk@orthlane.com>
2021-12-04 22:05:00 +0100

    add layout

--- utils/Layout.js
+++ utils/Layout.js
@@ -1,7 +1,10 @@
 import { View } from "react-native";
 import React from "react";
-const Layout = ({ children }) => {
-  return <View >{children}</View>;
+const Layout = ({ children,img,bool=false }) => {
+  return <View >
+     { img&&<Image source={img} style={{width:100,height:100}}/>}
+     { bool&&<Text>hello</Text>}
+      {children}</View>;
 };
 
 export default Layout;
