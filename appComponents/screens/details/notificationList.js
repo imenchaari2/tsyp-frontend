7d8d66e1a28fd6d6faa8fcd91ae4d0a87a798d20
Farouk <farouk@orthlane.com>
2021-12-04 23:56:19 +0100

    fix notitfcation push locally

--- appComponents/screens/details/notificationList.js
+++ appComponents/screens/details/notificationList.js
@@ -1,58 +1,95 @@
 const notificationList = [
-    {
-        title: "'December 03, 2021 18:29:00' ",
-        body: 'notification body',
-        trigger: new Date('December 03, 2021 23:10:30'),
-        data:{id:1},
+  {
+    title: "'December 03, 2021 18:29:00' ",
+    body: "notification body",
+    trigger: new Date("December 03, 2021 23:10:30"),
+    data: { id: 1 },
+  },
+  {
+    title: "'December 03, 2021 18:37:00' ",
+    body: "notification body",
+    trigger: new Date("December 03, 2021 23:10:40"),
+    data: { id: 2 },
+  },
+  {
+    title: "'December 03, 2021 18:37:00' ",
+    body: "notification body",
+    trigger: new Date("December 03, 2021 19:01:40"),
+    data: { id: 3 },
+  },
+  {
+    title: "taw' ",
+    body: "notification body",
+    trigger: new Date("December 04, 2021 15:18:00"),
+    data: {
+      screen: "Feedback",
+      id: 4,
+      image: `../../../assets/images/coronaReminder.png`,
     },
-    {
-        title: "'December 03, 2021 18:37:00' ",
-        body: 'notification body',
-        trigger: new Date('December 03, 2021 23:10:40'),
-        data:{id:2},
+  },
+  {
+    title: "taw' ",
+    body: "notification body",
+    trigger: new Date("December 04, 2021 15:23:00"),
+    data: {
+      screen: "Feedback",
+      image: "../../../assets/images/coronaReminder.png",
+      id: 5,
     },
-    {
-        title: "'December 03, 2021 18:37:00' ",
-        body: 'notification body',
-        trigger: new Date('December 03, 2021 19:01:40'),
-        data:{id:3},
+  },
+  {
+    title: "taw' ",
+    body: "notification body",
+    trigger: new Date("December 04, 2021 15:16:00"),
+    data: {
+      screen: "Feedback",
+      image: "../../../assets/images/coronaReminder.png",
+      id: 6,
     },
-    {
-        title: "taw' ",
-        body: 'notification body',
-        trigger: new Date('December 04, 2021 15:18:00'),
-        data:{screen:"Feedback",id:4,
-        image: `../../../assets/images/coronaReminder.png`,},
-       
 
-    },
-    {
-        title: "taw' ",
-        body: 'notification body',
-        trigger: new Date('December 04, 2021 15:23:00'),
-        data:{screen:"Feedback",image: require('../../../assets/images/coronaReminder.png'),id:5},
-
-
-    },   {
-        title: "taw' ",
-        body: 'notification body',
-        trigger: new Date('December 04, 2021 15:16:00'),
-        data:{screen:"Feedback",image: require('../../../assets/images/coronaReminder.png'),id:6},
-        image: require('../../../assets/images/coronaReminder.png'),
-
-    },
-    {
-        title: "'November 19, 2021 13:19:00' ",
-        body: 'notification body',
-        trigger: new Date('December 04, 2021 14:29:00'),
-        data:{id:7},
-    },
-    {
-        title: "'November 16, 2021 17:20:00'",
-        body: 'notification body',
-        trigger: new Date('December 03, 2021 23:16:40'),
-        data:{id:8},
-    }
+  },
+  {
+    title: "'November 19, 2021 13:19:00' ",
+    body: "notification body",
+    trigger: new Date("December 04, 2021 14:29:00"),
+    data: { id: 7 },
+  },
+  {
+    title: "'November 16, 2021 17:20:00'",
+    body: "notification body",
+    trigger: new Date("December 03, 2021 23:16:40"),
+    data: { id: 8 },
+  },
+  {
+    title: "'November 16, 2021 17:20:00'",
+    body: "notification body",
+    trigger: new Date("December 10, 2021 23:16:40"),
+    data: { id: 9 },
+  },
+  {
+    title: "'November 16, 2021 17:20:00'",
+    body: "notification body",
+    trigger: new Date("December 4, 2021 23:39:00"),
+    data: { id: 10 },
+  },
+  {
+    title: "'November 16, 2021 17:20:00'",
+    body: "notification body",
+    trigger: new Date("December 4, 2021 23:44:00"),
+    data: { id: 11 },
+  },
+  {
+    title: "'November 16, 2021 17:20:00'",
+    body: "notification body",
+    trigger: new Date("December 4, 2021 23:51:00"),
+    data: { id: 12 },
+  },
+  {
+    title: "'November 16, 2021 17:20:00'",
+    body: "notification body",
+    trigger: new Date("December 4, 2021 23:55:00"),
+    data: { id: 13 },
+  },
 ];
 
 export default notificationList;
